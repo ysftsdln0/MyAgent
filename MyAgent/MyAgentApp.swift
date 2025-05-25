9d4ad1229fb977b2d6c2abcc4dd1a1afc9ea5b99
ysftsdln0 <efe@tasdelen.net>
2025-05-25 23:07:39 +0300

    Initial Commit

---
+++ MyAgent/MyAgentApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAgentApp.swift
+//  MyAgent
+//
+//  Created by Yusuf Efe Taşdelen on 25.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct MyAgentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyAgentTests/MyAgentTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyAgentTests.swift
+//  MyAgentTests
+//
+//  Created by Yusuf Efe Taşdelen on 25.05.2025.
+//
+
+import Testing
+@testable import MyAgent
+
+struct MyAgentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
